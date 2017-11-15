368f29e3e2e98693f79f1e76891baebecd17a258
Jaap Abrahams <jaap_abrahams@yahoo.com>
2017-11-15 17:13:09 +0100

    apply es6 polyfill (browserify, babelify)

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,20 @@
-//es6
+//to make this work wrap the 'build' version (browseryfied, babelyfied etc. in gulp)
+//in function getAreas() {...}, which gets called in the google map script tag in the html
 const myJson = [];
-//googleError() only gets triggered if the FQDN is incorrect or if something else goes wrong. 
-//But if it's only the key which is incorrect then this function will not be triggered 
-//and the default google maps error notification will be shown 
+//googleError() only gets triggered if the FQDN is incorrect or if something else goes wrong.
+//But if it's only the key which is incorrect then this function will not be triggered
+//and the default google maps error notification will be shown
 function googleError() {
     ko.applyBindings(new errorViewModel());
 }
 
 function errorViewModel() {
-    this.googleErr = ko.observable('Sorry. Google Maps failed to load. Please try again later.');
+    this.googleErr = ko.observable("Sorry. Google Maps failed to load. Please try again later.");
     return false;
 }
 
 //pull THE MODEL data from the server
-function getAreas() {
+function getAreadata() {
     $.getJSON("https://api.myjson.com/bins/a90g1")
         .done(function(myJsonResp){
             const areaData = myJsonResp;
@@ -27,14 +28,16 @@ function getAreas() {
                     fill: area.fill,
                     img: area.img
                 });
-            };
+            }
             runApplication(myJson);
         })
         .fail(function(e){
-            alert('Something went wrong! Unable to load data to show the areas list.');
+            alert("Something went wrong! Unable to load data to show the areas list.");
         });
     return false;
 }
+
+getAreadata();
 
 function runApplication(myJson){
     //Note: fsqr = Foursquare
@@ -48,7 +51,7 @@ function runApplication(myJson){
     chosen,
     searchFor;
     let markersFsqr = [];
-    const img = 'img/pin.png';
+    const img = "img/pin.png";
     const center = {lat: 52.535722, lng: 13.414487};
     const myMarkers = [];
     const posMarkerFsqr = {};
@@ -66,7 +69,7 @@ function runApplication(myJson){
     };
 
     // Create a styles array to use with the map.
-    const styles = 
+    const styles =
 [{"featureType":"administrative.country","elementType":"geometry.fill","stylers":[{"color":"#98b9c5"}]},{"featureType":"administrative.province","elementType":"geometry.fill","stylers":[{"color":"#98b9c5"}]},{"featureType":"administrative.locality","elementType":"geometry.fill","stylers":[{"color":"#98b9c5"}]},{"featureType":"administrative.neighborhood","elementType":"geometry.fill","stylers":[{"color":"#98b9c5"}]},{"featureType":"administrative.land_parcel","elementType":"geometry.fill","stylers":[{"color":"#98b9c5"}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#9dbecb"}]},{"featureType":"landscape.natural.landcover","elementType":"geometry.fill","stylers":[{"color":"#96becd"}]},{"featureType":"landscape.natural.terrain","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"poi.attraction","elementType":"geometry.fill","stylers":[{"color":"#fff7dc"}]},{"featureType":"poi.business","elementType":"geometry.fill","stylers":[{"color":"#fff7dc"}]},{"featureType":"poi.business","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"poi.government","elementType":"geometry.fill","stylers":[{"color":"#fff7dc"}]},{"featureType":"poi.medical","elementType":"geometry.fill","stylers":[{"color":"#fff7dc"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#fff7dc"}]},{"featureType":"poi.place_of_worship","elementType":"geometry.fill","stylers":[{"color":"#ffc540"}]},{"featureType":"poi.school","elementType":"geometry.fill","stylers":[{"color":"#2d7488"}]},{"featureType":"poi.sports_complex","elementType":"geometry.fill","stylers":[{"color":"#9fdbea"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#7da4b3"},{"saturation":"0"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#b5cfd9"}]},{"featureType":"road.arterial","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.arterial","elementType":"labels.text.stroke","stylers":[{"visibility":"simplified"}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"#fbffcb"}]},{"featureType":"transit.line","elementType":"geometry.fill","stylers":[{"color":"#c9dbff"}]},{"featureType":"transit.station.airport","elementType":"geometry.fill","stylers":[{"color":"#beddff"}]},{"featureType":"transit.station.bus","elementType":"geometry.fill","stylers":[{"color":"#c0dff4"}]},{"featureType":"transit.station.rail","elementType":"geometry.fill","stylers":[{"color":"#00708b"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#79b7cd"}]}];
 
     map = new google.maps.Map(document.getElementById("map"), {
@@ -106,7 +109,7 @@ function runApplication(myJson){
         this.areaCoords = data.coords;
         this.fill = data.fill;
         this.areaImg = data.img;
-        this.myMarker = '';
+        this.myMarker = "";
     };
 
     //for each new Poi (a single search result) use this data
@@ -127,18 +130,18 @@ function runApplication(myJson){
         this.currentArea = ko.observable();
         this.chosenArea = ko.observable();
         this.fsqrFailText = ko.observable();
-        this.fsqrNoResult = ko.observable('');
+        this.fsqrNoResult = ko.observable("");
         this.showHideAreaList = ko.observable(true);
         this.showForm = ko.observable(true);
-        this.infoText1 = ko.observable('First...');
-        this.infoText2 = ko.observable('Then...');
+        this.infoText1 = ko.observable("First...");
+        this.infoText2 = ko.observable("Then...");
         this.showAgainBtn = ko.observable(false);
         const self = this;
 
         // walk through the model and fill the 'myAreas' array with all items in it
         for(const area of myJson) {
             self.myAreas.push( new Area(area) );
-        };
+        }
 
         //gets triggered after the foursquare response has been handled:
         this.setPois = (poiListing) => {
@@ -147,7 +150,7 @@ function runApplication(myJson){
             //walk through
             for(const poi of poiListing){
                 self.myPois.push( new Poi(poi) );
-            };
+            }
         };
 
         const bounceMarker = (marker) => {
@@ -183,8 +186,8 @@ function runApplication(myJson){
         this.itemListClick = (value) => {
             self.showAgainBtn(true);
             self.showForm(false);
-            self.infoText1('New search term?');
-            self.infoText2('New area?');
+            self.infoText1("New search term?");
+            self.infoText2("New area?");
             google.maps.event.trigger(value.myMarker, "click");
             //set the chosen area
             self.currentArea(self.myAreas()[value.areaId]);
@@ -198,7 +201,7 @@ function runApplication(myJson){
             map.panTo(centerOnItem);
 
             // check if a search term was entered and show/hide itemList and form
-            if(searchFor === '' || searchFor === undefined) {
+            if(searchFor === "" || searchFor === undefined) {
                 self.showItemList(false);
                 return false;
             }
@@ -208,10 +211,10 @@ function runApplication(myJson){
                     self.showHideAreaList(false);
                 }
             }
-            const latLng = chosen.loc.lat + ',' + chosen.loc.lng;
+            const latLng = chosen.loc.lat + "," + chosen.loc.lng;
 
         //store the search data in the url:
-        const fsqrUrl = 'https://api.foursquare.com/v2/venues/search?client_id=XV45PHON55RFFRM3GNN1BODO2NWH45LCGBKPVQWLOKGUI4XA&client_secret=YVKQJGWU1G2TH1BGQXHULAGHOYDL443Z3HPQ0JBFCJWM4QPD&v=20130815&ll=' + latLng + '&query=' + searchFor + '&intent=checkin&limit=5&radius=1000';       
+        const fsqrUrl = "https://api.foursquare.com/v2/venues/search?client_id=XV45PHON55RFFRM3GNN1BODO2NWH45LCGBKPVQWLOKGUI4XA&client_secret=YVKQJGWU1G2TH1BGQXHULAGHOYDL443Z3HPQ0JBFCJWM4QPD&v=20130815&ll=" + latLng + "&query=" + searchFor + "&intent=checkin&limit=5&radius=1000";
             //foursquare jquery json request:
             $.getJSON(fsqrUrl, function(data){
                 const dataResp = data.response.venues;
@@ -234,11 +237,11 @@ function runApplication(myJson){
                 //check if there are any results, if not, notify
                 const checkResult = (fsqrData) => {
                     if(fsqrData.length === 0){
-                        self.fsqrNoResult('Sorry, no results found!');
+                        self.fsqrNoResult("Sorry, no results found!");
                         self.showResults(false);
                     }
                     else {
-                        self.fsqrNoResult('');
+                        self.fsqrNoResult("");
                         self.showResults(true);
                         }
                 };
@@ -248,17 +251,17 @@ function runApplication(myJson){
                 // some returned values are 'undefined', replace them with ''
                 for(const value of fsqrData){
                     if(value.url === undefined) {
-                        value.url = '';
+                        value.url = "";
                     }
                     if(value.phone === undefined) {
-                        value.phone = '';
+                        value.phone = "";
                     }
                     // formattedAddress is an array of three:
                     for(let i = 0; i < 3; i++){
                         if(value.address[i] === undefined){
-                            value.address[i] = '';
+                            value.address[i] = "";
                         }
-                    }                
+                    }
                 };
 
                 //FOURSQUARE on the map:
@@ -282,7 +285,7 @@ function runApplication(myJson){
                         addressMarkerFsqr.push(value.address);
                         phoneMarkerFsqr.push(value.phone);
                         urlMarkerFsqr.push(value.url);
-                        iconMarkerFsqr = value.iconPre + 'bg_32' + value.iconSuf;
+                        iconMarkerFsqr = value.iconPre + "bg_32" + value.iconSuf;
                         markerFsqr = new google.maps.Marker({
                             position: posMarkerFsqr,
                             map: map,
@@ -297,12 +300,12 @@ function runApplication(myJson){
                             ${addressMarkerFsqr[index][2]}
                             ${phoneMarkerFsqr[index]}
                             <a title="This link will open in a new tab" href = " ${urlMarkerFsqr[index]} " target="_blank" >
-                            ${urlMarkerFsqr[index]}</a></p></div>`;                       
+                            ${urlMarkerFsqr[index]}</a></p></div>`;
                             return contentString;
                         };
 
                         // add click listener to the foursquare markers to show the foursquare info
-                        google.maps.event.addListener(markerFsqr, 'click', (function(markerFsqr, index) {
+                        google.maps.event.addListener(markerFsqr, "click", (function(markerFsqr, index) {
                             const content = getFsqrContent(index);
                             return function() {
                             bounceMarker(markerFsqr);
@@ -326,14 +329,14 @@ function runApplication(myJson){
                 self.setMapOnAll(map);
             })
             .fail(function(e){
-                self.fsqrFailText('Sorry. Something has gone wrong. The Foursquare server could not be reached');
+                self.fsqrFailText("Sorry. Something has gone wrong. The Foursquare server could not be reached");
             });//end getJson
             return false;
         };//end itemListClick()
-                    
+
         //set content for the area infoWindow
         function getContent(area){
-            let showImg = '';
+            let showImg = "";
             if(vwHeight() >= 500){ showImg = `<img src = "${area.areaImg}">`; }
             const contentString = `<div class="infoWindow"><p class="infoWindowHeading">${area.areaName}</p>
             <p>${area.areaInfo}
@@ -343,12 +346,12 @@ function runApplication(myJson){
         let poly;
         let polygons = [];
 
-        //place polygons and area markers on the map 
+        //place polygons and area markers on the map
         for(const area of self.myAreas()){
             //make and place the polygons:
             poly = new google.maps.Polygon({
                 paths: area.areaCoords,
-                strokeColor: 'black',
+                strokeColor: "black",
                 strokeOpacity: 0.8,
                 strokeWeight: 0.5,
                 fillColor: area.fill,
@@ -378,38 +381,37 @@ function runApplication(myJson){
             });
         }
 
-
       // Close infowindow when clicked elsewhere on the map, including the polygons
         map.addListener("click", function(){
             infowindow.close(infowindow);
         });
 
         for(const value of polygons){
-            google.maps.event.addListener(value, 'click', (function(){
+            google.maps.event.addListener(value, "click", (function(){
                 infowindow.close(infowindow);})
             );
         }
 
         // filter for the areaList
-        self.filter = ko.observable('');
+        self.filter = ko.observable("");
 
         this.areaList = ko.computed(function() {
-			let q = this.filter().toLowerCase();
-			if (!q) {
-				return ko.utils.arrayFilter(self.myAreas(), function(item) {
-					item.myMarker.setVisible(true);
-					return true;
-          		});
+            let q = this.filter().toLowerCase();
+            if (!q) {
+                return ko.utils.arrayFilter(self.myAreas(), function(item) {
+                    item.myMarker.setVisible(true);
+                    return true;
+                });
             } else {
-	            return ko.utils.arrayFilter(this.myAreas(), function(item) {
-	                if (item.areaName.toLowerCase().indexOf(q) >= 0) {
-					item.myMarker.setVisible(true);
-	                    return true;
-	                } else {
-						item.myMarker.setVisible(false);
-						return false;
-	                }
-	            });
+                return ko.utils.arrayFilter(this.myAreas(), function(item) {
+                    if (item.areaName.toLowerCase().indexOf(q) >= 0) {
+                    item.myMarker.setVisible(true);
+                        return true;
+                    } else {
+                        item.myMarker.setVisible(false);
+                        return false;
+                    }
+                });
             }
         }, this);
     }//end appViewModel
